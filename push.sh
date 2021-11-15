73e05ea9d6303e548638aebf025df11b908d31aa
Stefan Moonen <stefan.moonen@gmail.com>
2021-11-15 14:28:57 +0100

    Split pushing images to shell script

---
+++ push.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -eE -o functrace
+
+failure() {
+  local lineno=$1
+  local msg=$2
+  echo "Failed at $lineno: $msg"
+}
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+
+BRANCH_VARIANT=`echo "$VARIANT" | sed 's/\./-/g'`
+
+docker push qonstrukt/php:${PHP_VERSION}-v4-slim-${BRANCH_VARIANT}
+docker push qonstrukt/php:${PHP_VERSION}-v4-${BRANCH_VARIANT}
+docker push qonstrukt/php:${PHP_VERSION}-v4-${BRANCH_VARIANT}-node10
+docker push qonstrukt/php:${PHP_VERSION}-v4-${BRANCH_VARIANT}-node12
+docker push qonstrukt/php:${PHP_VERSION}-v4-${BRANCH_VARIANT}-node14
+docker push qonstrukt/php:${PHP_VERSION}-v4-${BRANCH_VARIANT}-node16
+
+# Let's also tag PHP patch releases
+PHP_PATCH_VERSION=`docker run --rm qonstrukt/php:${PHP_VERSION}-v4-slim-${BRANCH_VARIANT} php -v | head -n1 | grep -P '\d+\.\d+\.\d+' -o | head -n1`
+echo "Tagging patch release $PHP_PATCH_VERSION"
+docker tag qonstrukt/php:${PHP_VERSION}-v4-slim-${BRANCH_VARIANT} qonstrukt/php:${PHP_PATCH_VERSION}-v4-slim-${BRANCH_VARIANT}
+docker tag qonstrukt/php:${PHP_VERSION}-v4-${BRANCH_VARIANT} qonstrukt/php:${PHP_PATCH_VERSION}-v4-${BRANCH_VARIANT}
+docker tag qonstrukt/php:${PHP_VERSION}-v4-${BRANCH_VARIANT}-node14 qonstrukt/php:${PHP_PATCH_VERSION}-v4-${BRANCH_VARIANT}-node10
+docker tag qonstrukt/php:${PHP_VERSION}-v4-${BRANCH_VARIANT}-node14 qonstrukt/php:${PHP_PATCH_VERSION}-v4-${BRANCH_VARIANT}-node12
+docker tag qonstrukt/php:${PHP_VERSION}-v4-${BRANCH_VARIANT}-node14 qonstrukt/php:${PHP_PATCH_VERSION}-v4-${BRANCH_VARIANT}-node14
+docker tag qonstrukt/php:${PHP_VERSION}-v4-${BRANCH_VARIANT}-node14 qonstrukt/php:${PHP_PATCH_VERSION}-v4-${BRANCH_VARIANT}-node16
+docker push qonstrukt/php:${PHP_PATCH_VERSION}-v4-slim-${BRANCH_VARIANT}
+docker push qonstrukt/php:${PHP_PATCH_VERSION}-v4-${BRANCH_VARIANT}
+docker push qonstrukt/php:${PHP_PATCH_VERSION}-v4-${BRANCH_VARIANT}-node10
+docker push qonstrukt/php:${PHP_PATCH_VERSION}-v4-${BRANCH_VARIANT}-node12
+docker push qonstrukt/php:${PHP_PATCH_VERSION}-v4-${BRANCH_VARIANT}-node14
+docker push qonstrukt/php:${PHP_PATCH_VERSION}-v4-${BRANCH_VARIANT}-node16
