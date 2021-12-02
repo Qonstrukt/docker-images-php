c89a987289acf74bfb8316a3541da44f21662991
Stefan Moonen <stefan.moonen@gmail.com>
2021-12-02 16:48:14 +0100

    Move tini back to root

--- utils/docker-entrypoint.sh
+++ utils/docker-entrypoint.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-exec "sudo" "-E" "/usr/local/bin/tini" "-g" "-s" "--" "/usr/local/bin/docker-entrypoint-as-root.sh" "$@";
+exec "sudo" "-E" "/tini" "-g" "-s" "--" "/usr/local/bin/docker-entrypoint-as-root.sh" "$@";
