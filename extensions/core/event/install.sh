0f98913ce21980870583cb3db3f9a4d54a522841
Stefan Moonen <stefan.moonen@gmail.com>
2022-07-14 11:10:27 +0200

    Use libssl 3.0 when compiling php-event

--- extensions/core/event/install.sh
+++ extensions/core/event/install.sh
@@ -6,6 +6,6 @@ set -e
 export USE_PECL=1
 export PECL_EXTENSION="event"
 export DEV_DEPENDENCIES="libevent-dev libssl-dev"
-export DEPENDENCIES="libevent-2.1-7 libevent-core-2.1-7 libevent-extra-2.1-7 libevent-openssl-2.1-7 libevent-pthreads-2.1-7 libssl1.1"
+export DEPENDENCIES="libevent-2.1-7 libevent-core-2.1-7 libevent-extra-2.1-7 libevent-openssl-2.1-7 libevent-pthreads-2.1-7 libssl"
 
 ../docker-install.sh
